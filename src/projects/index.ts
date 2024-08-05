03a47ca0e94cf8b055dc741ce829baca7b4991ec
Abhi A <85126640+TalkativeDiv@users.noreply.github.com>
2024-08-05 16:31:57 -0400

    removed dist

---
+++ src/projects/index.ts
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    name: "Fullstack Gallery App",
+    description:
+      "A fullstack gallery app built with NextJS, Typescript, Clerk, Drizzle ORM, Vercel Postgres, Shadcn UI, Uploadthing and Upstash.",
+    github: "https://github.com/TalkativeDiv/t3gallery",
+    url: "https://t3gallery-psi-azure.vercel.app/",
+  },
+  {
+    name: "Fullstack Expense Tracker",
+    description:
+      "A fullstack expense tracker app built with NextJS, Typescript, Prisma ORM, Neon Postgres, and Shadcn UI.",
+    github: "https://github.com/TalkativeDiv/expense-tracker",
+    url: "https://expense-tracked.vercel.app/",
+  },
+  {
+    name: "Fullstack Blog App",
+    description:
+      "A fullstack blog app built with HonoJS, Typescript, Drizzle ORM, SQLite, Bootstrap UI, Bun, Lucia, and HTMX.",
+    github: "https://github.com/TalkativeDiv/hono-blog-app",
+    url: "https://hono-blog-app.talkativediv.workers.dev/",
+  },
+  {
+    name: "Landing Page",
+    description:
+      "A Landing Page built with NextJS, TailwindCSS, and Framer Motion.",
+    github: "https://github.com/TalkativeDiv/metaversus",
+    url: "https://metaversus-design.vercel.app/",
+  },
+];
+
+export default projects;
