95807c94f8b2ce617a05eeb6163da491b3e2c920
abhi <85126640+TalkativeDiv@users.noreply.github.com>
2024-09-28 20:06:12 -0400

    blog post: astro theme switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,29 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import partytown from "@astrojs/partytown";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   // output: "server",
-  integrations: [
-    react(),
-    tailwind(),
-    // partytown({
-    //   forward: ["dataLayer.push"],
-    // }),
-  ],
+  integrations: [react(), tailwind()
+  // partytown({
+  //   forward: ["dataLayer.push"],
+  // }),
+  , mdx()],
   image: {
-    remotePatterns: [{ protocol: "https" }],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "catppuccin-mocha",
       wrap: true,
-      transformers: [],
-    },
-  },
+      transformers: []
+    }
+  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,5 @@ const blog = defineCollection({
   }),
 });
 
+
 export const collections = { blog };
